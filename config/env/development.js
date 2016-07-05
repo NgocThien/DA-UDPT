c9d0f8546745c4ffc48a5fb0928444da7fa10958
1312680 <1312680@student.hcmus.edu.vn>
2016-07-05 15:11:01 +0700

    fix

--- config/env/development.js
+++ config/env/development.js
@@ -1,7 +1,7 @@
 module.exports = {
 	'facebookAuth' : {
-	FB_CLIENT_ID : '165598543841093',
-	FB_SECRET_ID :'594c88a87cda8da1165fcdd4336ba9c8',
+	clientID : '165598543841093',
+	clientSecret :'594c88a87cda8da1165fcdd4336ba9c8',
 	'callbackURL' : 'http://apps-blog3t.herokuapp.com/auth/facebook/callback'
 	}
 
