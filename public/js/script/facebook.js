f6eba22e99805f0fef94b5798b267a07f13867bd
1312680 <1312680@student.hcmus.edu.vn>
2016-07-05 15:56:04 +0700

    fix

--- public/js/script/facebook.js
+++ public/js/script/facebook.js
@@ -2,7 +2,7 @@
         var js, fjs = d.getElementsByTagName(s)[0];
         if (d.getElementById(id)) return;
         js = d.createElement(s); js.id = id;
-        js.src = "//connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v2.6&appId=1750090615264037";
+        js.src = "//connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v2.6&appId=165598543841093";
          fjs.parentNode.insertBefore(js, fjs);
       }(document, 'script', 'facebook-jssdk'));
       
